8e9efcca8ffaf49cfb1a949b712e011cf1fbf0b1
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-03-22 16:40:04 +0800

    Login Window Open and Close

--- src/misc/header.js
+++ src/misc/header.js
@@ -3,6 +3,8 @@ import headerlogo from '../header-logo.svg'
 
 function Header() {
 
+    const [LoginWindow, setLoginWindow] = useState(false)
+
     const [loggedIn, setLoggedIn] = useState({
         loggedIn: false,
         username: ''
@@ -10,9 +12,24 @@ function Header() {
 
     return (
         <span>
+            {LoginWindow ? <div className="LoginWindow">
+                <div style={{
+                    position: "absolute",
+                    right: "15px",
+                    fontSize: "25px",
+                    color: "#ff5f5f",
+                    cursor: "pointer",
+                    userSelect: "none",
+                }} onClick={() => {setLoginWindow(false)}}>X</div>
+                <h2>Login</h2>
+                <input type="text" placeholder="Username" /><br />
+                <input type="text" placeholder="Password" /><br />
+                <button>Login</button><br />
+                <sub>Don't have an account? Register here!</sub>
+            </div> : null}
             <div className="Header">
                 <img className='headerlogo' src={headerlogo} />
-                <div className="header-login-btn">Login</div>
+                <div className="header-login-btn" onClick={() => {setLoginWindow(true)}}>Login</div>
             </div>
             <br />
             <br />
