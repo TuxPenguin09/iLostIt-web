db602e5156fa0a6d6a1b26c374a0e13e345b5f62
TimeDev <2021sha01015@iacademy.edu.ph>
2023-03-23 10:07:53 +0800

    Put and Edited texts

--- src/page/register.jsx
+++ src/page/register.jsx
@@ -4,7 +4,10 @@ import './register.css'
 function RegisterPage() {
     return (
         <div>
-            <h2>Register</h2>
+            <h2 id="ter">Register</h2>
+            <p id="me">Username:</p>
+            <p id="rd">Password:</p>
+            <p id="rm">Confirm Password:</p>
         </div>
     )
 }
