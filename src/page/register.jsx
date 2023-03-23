5d587ada70e1a00794e521a010e8ee11748d1989
TimeDev <2021sha01015@iacademy.edu.ph>
2023-03-23 09:02:14 +0800

    Ïnit Register Page

---
+++ src/page/register.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './register.css'
+
+function RegisterPage() {
+    return (
+        <div>
+            <h2>Register</h2>
+        </div>
+    )
+}
+
+export default RegisterPage
