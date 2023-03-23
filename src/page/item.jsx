15651bc491ca580d91842f7cca5f9eccec8d50a5
TimeDev <2021sha01015@iacademy.edu.ph>
2023-03-23 08:23:27 +0800

    Added code for individual items

--- src/page/item.jsx
+++ src/page/item.jsx
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
 
+const itemlos=(props)=>
+{
+    return(<div>
+        <p>Time Found:{null}</p>
+        <p>Finder Name:{null}</p>
+        <p>Item:{null}</p>
+        <img src={null} alt="Lost Item"/>
+        </div>);
+};
+
 function ItemPage(props) {
     return (
         <div>
             <h2>Item</h2>
             {/*Insert your HTML code here*/}
+            {itemlos()}
         </div>
     )
 }
