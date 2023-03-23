23c197f1eb4ec620e7312b1ddd9e69acde3b4c11
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-03-23 09:25:30 +0800

    Contact Founder

--- src/page/item.jsx
+++ src/page/item.jsx
@@ -7,7 +7,7 @@ function ItemPage(props) {
             <h2>Item</h2>
             <img id="itempage-image" src={null} alt="Lost Item" />
             <p>Lost since {null} founded by {null}</p>
-
+            <div id="itempage-sendmessage">Conatct Founder</div>
 
         </div>
     )
