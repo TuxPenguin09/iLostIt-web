e3d89bad90b3571121cccaab0b943bd62a71149d
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-03-23 09:06:57 +0800

    Lost Item Style Fix

--- src/page/item.jsx
+++ src/page/item.jsx
@@ -5,13 +5,10 @@ function ItemPage(props) {
     return (
         <div className="ItemPage">
             <h2>Item</h2>
-            <div>
-                <img id="itempage-image" src={null} alt="Lost Item" />
-                <p id="time_found-item">Time Found:{null}</p>
-                <p id="findername">Finder Name:{null}</p>
-                <p id="em">Item:{null}</p>
-                
-            </div>
+            <img id="itempage-image" src={null} alt="Lost Item" />
+            <p>Lost since {null} founded by {null}</p>
+
+
         </div>
     )
 }
