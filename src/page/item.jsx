713c7200549e4db39b1da60522187dfab942693f
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-03-26 12:24:42 +0800

    Item Page with Backend

--- src/page/item.jsx
+++ src/page/item.jsx
@@ -1,32 +1,75 @@
 import React, { useEffect, useState } from 'react';
 import './item.css'
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 const hostname = process.env.REACT_APP_HOSTNAME;
-
 
 function ItemPage(props) {
 
     const { "*": itemid } = useParams();
 
+    const [loading, setLoading] = useState({
+        loading: true,
+        error: false,
+    })
+
+    const [itemdet, setitemdet] = useState({
+        title: '',
+        founder: '',
+        date: '',
+        image: '',
+    })
+
     useEffect(() => {
         console.log(itemid)
+        axios.get(`${hostname}/items/item?itemid=${itemid}`, {
+            'headers': {
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            }
+        })
+            .then(res => {
+                console.log(res.data)
+                setitemdet({
+                    title: res.data.item_name,
+                    founder: res.data.username,
+                    date: res.data.lost_since,
+                    image: res.data.image,
+                })
+                setLoading({
+                    loading: false,
+                    error: false,
+                })
+            })
+            .catch(err => {
+                console.log(err)
+                setLoading({
+                    loading: false,
+                    error: true,
+                })
+            })
     }, [itemid])
-    
+
     return (
         <div className="ItemPage">
-            <h2>Item</h2>
-            <span style={{
-                backgroundColor: "white",
-                display: "block",
-                padding: "15px",
-                width: "450px",
-                borderRadius: "7px"
-            }}>
-                <img id="itempage-image" src={null} alt="Lost Item" />
-                <p style={{color: "black"}}>Lost since {""} founded by {""}</p>
-            </span>
-            <br />
-            <div id="itempage-sendmessage">Contact Facilities Department</div>
+            {loading.loading ? (<div className="loading">Loading...</div>) : (loading.error ? (
+                <span>
+                    <div>No Item found or an error occurred</div>
+                </span>
+            ) : (<span>
+                <h2>{itemdet.title}</h2>
+                <span style={{
+                    backgroundColor: "white",
+                    display: "block",
+                    padding: "15px",
+                    width: "450px",
+                    borderRadius: "7px"
+                }}>
+                    <img id="itempage-image" src={`${hostname}/cdn/image?img=${itemdet.image}`} alt="Lost Item" />
+                    <p style={{ color: "black" }}>Lost since {itemdet.date} founded by {itemdet.founder}</p>
+                </span>
+                <br />
+                <div id="itempage-sendmessage">Contact Facilities Department</div>
+            </span>))}
         </div>
     )
 }
