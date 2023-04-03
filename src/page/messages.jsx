7d8077dcdcd8b4941757247c682343d0a04dd43f
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-04-04 07:56:03 +0800

    Messages Skeleton

--- src/page/messages.jsx
+++ src/page/messages.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './register.css'
 import axios from 'axios';
 import { useParams, Link } from 'react-router-dom';
@@ -6,9 +6,30 @@ import { useParams, Link } from 'react-router-dom';
 const hostname = process.env.REACT_APP_HOSTNAME;
 
 export function MessagesPage(props) {
+    const [messagelist, setMessageList] = useState([]);
+
+    useEffect(() => {
+        axios.get(`${hostname}/messages/list`, {
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("token")}`
+            }
+        }).then(res => {
+            setMessageList(res.data)
+        })
+    })
+
+    function MessageList() {
+        return (<div>
+            
+        </div>)
+    }
+
     return (
         <div style={{padding: "10px"}}>
             <h2>Messages</h2>
+            {messagelist.map((message, index) => {
+
+            })}
         </div>
     )
 }
@@ -17,10 +38,17 @@ export function MessagesPage(props) {
 export function DirectMessagePage(props) {
     const { ":item": itemid } = useParams();
     const { ":user": userid } = useParams();
-    
+
+    const [messages, setMessages] = useState();
+
+    function message() {
+
+    }
+
     return (
         <div style={{padding: "10px"}}>
             <h2>Message</h2>
+
         </div>
     )
 }
